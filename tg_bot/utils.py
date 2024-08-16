0d7dadabcac473cd15776e62374effdc69f5edbc
rocinantt <s.s.serebryakov@yandex.ru>
2024-08-16 20:05:11 +0300

    fix func in tg_bot

--- tg_bot/utils.py
+++ tg_bot/utils.py
@@ -88,7 +88,7 @@ async def send_results(message: types.Message, results, offset):
         await message.answer(text, parse_mode=ParseMode.HTML)
 
     if len(results) > 0:
-        await message.answer(' ', reply_markup=get_more_results_markup())
+        await message.answer('q', reply_markup=get_more_results_markup())
     else:
         builder = InlineKeyboardBuilder()
         builder.button(text='Начать заново', callback_data='start')

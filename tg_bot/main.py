b1f78b0be8c53415d26673eef7c0ae5ed4094953
rocinantt <s.s.serebryakov@yandex.ru>
2024-08-23 16:37:19 +0300

    fix tg_bot

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -9,14 +9,14 @@ from handlers import register_handlers
 try:
     bot = Bot(token=API_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
-    logging.info("Бот и диспетчер успешно инициализированы.")
+    logger.info("Бот и диспетчер успешно инициализированы.")
 except Exception as e:
-    logging.error(f"Ошибка инициализации бота или диспетчера: {e}")
+    logger.error(f"Ошибка инициализации бота или диспетчера: {e}")
     exit(1)
 
 # Регистрация обработчиков
 register_handlers(dp)
 
 if __name__ == '__main__':
-    logging.info("Запуск бота")
+    logger.info("Запуск бота")
     dp.run_polling(bot, polling_timeout=3)

f839380a01246026feb5eeebab1f579b2d8ac372
rocinantt <s.s.serebryakov@yandex.ru>
2024-08-26 14:10:01 +0300

    batch extract features in primary_parser

--- primary_parser/image_processing.py
+++ primary_parser/image_processing.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import numpy as np
 from PIL import Image
 import torch
 from sklearn.preprocessing import normalize
@@ -13,6 +14,7 @@ model = ViTForImageClassification.from_pretrained(model_path)
 
 # Настройка модели для использования только экстрактора признаков
 model.classifier = torch.nn.Identity()
+
 
 def load_image(url):
     """
@@ -34,7 +36,8 @@ def load_image(url):
         logger.error(f"Ошибка при обработке изображения: {e}")
         raise
 
-def extract_features(image_tensors):
+
+def extract_features_batch(image_tensors):
     """
     Извлекает признаки из батча тензоров изображений.
 
@@ -57,7 +60,7 @@ def normalize_features(features):
     :param features: векторы признаков
     :return: нормализованные векторы признаков
     """
-    return normalize(features.reshape(1, -1)).flatten()
+    return normalize(features, axis=1)
 
 
 def process_post(post):
@@ -65,22 +68,35 @@ def process_post(post):
     Обрабатывает пост для извлечения признаков изображений.
 
     :param post: пост с изображениями
-    :return: список признаков изображений
+    :return: список нормализованных признаков изображений
     """
     post_features = []
+    image_tensors_list = []
+
     try:
+        # Загрузка всех изображений и сбор в один батч
         for photo_url in post['photos']:
             image_tensor = load_image(photo_url)
             if image_tensor is None:
                 logger.error(f"Пропуск изображения по URL {photo_url} из-за ошибки загрузки.")
                 continue
-            features = extract_features(image_tensor).squeeze().cpu().numpy()
-            if features.size == 0:
-                logger.error(f"Пропуск изображения по URL {photo_url} из-за ошибки извлечения признаков.")
-                continue
+            image_tensors_list.append(image_tensor)
 
-            # Нормализация признаков перед добавлением в список
-            normalized_features = normalize_features(features)
+        if len(image_tensors_list) == 0:
+            logger.error(f"Все изображения в посте {post['post_id']} не удалось загрузить.")
+            return []
+
+        # Объединяем все тензоры в один батч
+        image_tensors_batch = torch.cat(image_tensors_list, dim=0)
+
+        # Извлекаем признаки сразу для всего батча
+        features_batch = extract_features_batch(image_tensors_batch).cpu().numpy()
+
+        # Нормализуем все признаки в батче
+        normalized_features_batch = normalize_features(features_batch)
+
+        # Сохраняем нормализованные признаки для каждого изображения
+        for normalized_features in normalized_features_batch:
             post_features.append(normalized_features)
 
         return post_features
